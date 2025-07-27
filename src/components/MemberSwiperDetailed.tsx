149d6dc7aa27f9f5d1d382fa56e88b2c5ed86cde
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-07-26 21:27:52 -0500

    feat: ideasSolutios en/es/por

--- src/components/MemberSwiperDetailed.tsx
+++ src/components/MemberSwiperDetailed.tsx
@@ -67,6 +67,7 @@ export default function TeamCarouselDetailed({
         roleDetailed={roleDetailed}
         description={description}
         image={image}
+        translations={translations}
       />
     );
   };
