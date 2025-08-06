03974be9fccff8dcb1e5aebb3979ab831c32b93a
Germán Rojas <garcejr@gmail.com>
2025-08-06 10:32:05 -0500

    feat: add firebase initialization and Firestore setup

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqeitkVnslmcclqcUaD5oTF2KoISfgnkw",
+  authDomain: "elipson-uat.firebaseapp.com",
+  projectId: "elipson-uat",
+  storageBucket: "elipson-uat.firebasestorage.app",
+  messagingSenderId: "585838521730",
+  appId: "1:585838521730:web:f719ba23e5af71951cb39b",
+  measurementId: "G-J5QL2P7PRZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
