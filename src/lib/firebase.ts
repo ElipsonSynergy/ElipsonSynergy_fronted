f3593baac3b25d7f714023840a83732e27cf4173
Feru_34 <f.rueda4@uniandes.edu.co>
2025-08-09 17:42:07 -0500

    BlogDetail working with Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,16 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import {
-  getFirestore,
+import { getFirestore } from 'firebase/firestore/lite';
+
+// Re-exporta helpers (¡clave!)
+export {
   collection,
   getDocs,
-  query,
-  orderBy,
-  doc,
   getDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  doc,
 } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
@@ -18,6 +22,15 @@ const firebaseConfig = {
   appId: "1:585838521730:web:f719ba23e5af71951cb39b",
   measurementId: "G-J5QL2P7PRZ"
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+// };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
