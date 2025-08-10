c949c7c9401c25f7f036f848a81782b44d6f3f02
Feru_34 <f.rueda4@uniandes.edu.co>
2025-08-10 15:20:19 -0500

    Deploy 6

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,24 +13,24 @@ export {
   doc,
 } from 'firebase/firestore/lite';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBqeitkVnslmcclqcUaD5oTF2KoISfgnkw",
-  authDomain: "elipson-uat.firebaseapp.com",
-  projectId: "elipson-uat",
-  storageBucket: "elipson-uat.firebasestorage.app",
-  messagingSenderId: "585838521730",
-  appId: "1:585838521730:web:f719ba23e5af71951cb39b",
-  measurementId: "G-J5QL2P7PRZ"
-};
-
 // const firebaseConfig = {
-//   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+//   apiKey: "AIzaSyBqeitkVnslmcclqcUaD5oTF2KoISfgnkw",
+//   authDomain: "elipson-uat.firebaseapp.com",
+//   projectId: "elipson-uat",
+//   storageBucket: "elipson-uat.firebasestorage.app",
+//   messagingSenderId: "585838521730",
+//   appId: "1:585838521730:web:f719ba23e5af71951cb39b",
+//   measurementId: "G-J5QL2P7PRZ"
 // };
+
+const firebaseConfig = {
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+};
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
